005bb83dd631e986547501b5e1df24453c418076
Ahnsukjoo <bos98@naver.com>
2025-01-04 18:51:49 +0900

    1/4

--- src/main/java/me/shinsunyoung/springbootdeveloper/config/WebSecurityConfig.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/config/WebSecurityConfig.java
@@ -1,4 +1,6 @@
 package me.shinsunyoung.springbootdeveloper.config;
+/*
+package me.shinsunyoung.springbootdeveloper.config;
 
 import lombok.RequiredArgsConstructor;
 import me.shinsunyoung.springbootdeveloper.service.UserDetailService;
@@ -57,5 +59,75 @@ public class WebSecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+}*/
+
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+import lombok.RequiredArgsConstructor;
+import me.shinsunyoung.springbootdeveloper.config.jwt.TokenProvider;
+import me.shinsunyoung.springbootdeveloper.repository.RefreshTokenRepository;
+import me.shinsunyoung.springbootdeveloper.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebSecurityConfig {
+
+    private final OAuth2UserCustomerService oAuth2UserCustomerService;
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserService userService;
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+            .requestMatchers(toH2Console())
+            .requestMatchers(new AntPathRequestMatcher("/img/**"),
+                new AntPathRequestMatcher("/css/**"),
+                new AntPathRequestMatcher("/js/**")
+            );
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .logout(AbstractHttpConfigurer::disable)
+            .sessionManagement(
+                management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(tokenAuthenticationFilter(),
+                UsernamePasswordAuthenticationFilter.class)
+            .authorizeRequests(auth -> auth
+                .requestMatchers(new AntPathRequestMatcher("/api/token")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api/**")).authenticated()
+                .anyRequest().permitAll())
+            .oauth2Login(oauth2 -> oauth2.loginPage("/login")
+                .authorizationEndpoint(
+                    authorizationEndpoint -> authorizationEndpoint.authorizationRequestRepository(
+                        oAuth2AuthorizationRequstBasedOnCookieRepository()))
+                .userInfoEndpoint(
+                    userIntoEndpoint -> userIntoEndpoint.userService(oAuth2UserCustomerService))
+                .successHandler(oAuth2SuccessHandler())
+            )
+            .exceptionHandling(
+                exceptionHandling -> exceptionHandling.defaultAuthenticationEntryPointFor(
+                    new HttpStratusEntryPoint(
+                        HttpStatus.UNAUTHORIZED), new AntPathRequestMatcher("/api/**")
+                ))
+            .build();
+
     }
 }
